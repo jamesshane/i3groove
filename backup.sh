08ee9f87e2f6ab07fb1af2e7321395157d3d2efe
James Shane <jamesshane@gmail.com>
2018-06-12 00:13:42 -0600

    bachup.sh added to rep

--- backup.sh
+++ backup.sh
@@ -2,4 +2,5 @@
 ### Script written by James Shane ( github.com/jamesshane )
 
 cd src
-python i3wm-themer.py --config config.yaml --install defaults/
+mkdir $HOME/Backup
+python i3wm-themer.py --config config.yaml --backup $HOME/Backup
