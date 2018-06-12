74ac7b6767dc712da00ea465c852f0ec5b1f109e
James Shane <jamesshane@gmail.com>
2018-06-12 00:13:21 -0600

    added default script copy over

--- defaults.sh
+++ defaults.sh
@@ -2,4 +2,5 @@
 ### Script written by James Shane ( github.com/jamesshane )
 
 cd src
+cp -r ../scripts/* /home/$USER/.config/polybar/
 python i3wm-themer.py --config config.yaml --install defaults/
